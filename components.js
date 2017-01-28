4b180f0d004d0620c98c9f12d312d8c374cd0010
Esko Cruzeta <eskocruz@users.noreply.github.com>
2017-01-28 11:56:39 -0800

    JSX React StoryBox Component and Render

---
+++ components.js
@@ -0,0 +1,12 @@
+/**
+ * Created by esko on 1/28/17.
+ */
+class StoryBox extends React.Component {
+    render() {
+        return ( <div>Story Box</div> );
+    }
+}
+
+ReactDOM.render(
+    <StoryBox/>, document.getElementById('story-app')
+);
